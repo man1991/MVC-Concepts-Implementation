8a2c7ffd6a67198ab35dfc0ca4b7e18ebdeb24e3
man1991 <man.222.sin@gmail.com>
2019-08-27 00:18:58 +0530

    Implemented ViewModel, Partial Views and WebGrid.
    Added Customer ViewModel to show data on Enter Customer screen in grid form also created grid in HTML and further used WebGrid to make it look more generic.
    Added functionality SearchCustomer to search customer using name and displaying data to on same page
    n grid form also created grid in HTML and further used WebGrid to make it look more generic.
    Further, implemented Partial View to transfer common grid code to DisplayCustomer which helps to reuse code functionality.

--- HelloWorld/HelloWorld/Controllers/CustomerController.cs
+++ HelloWorld/HelloWorld/Controllers/CustomerController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using HelloWorld.Models;
 using HelloWorld.DAL;
+using HelloWorld.ViewModel;//refernce view model class
 
 namespace HelloWorld.Controllers
 {
@@ -40,27 +41,105 @@ namespace HelloWorld.Controllers
         }
         public ActionResult Enter()
         {
-            return View("EnterCustomer", new Customer());
+            //view model object
+            CustomerViewModel customerViewModel = new CustomerViewModel();
+
+            //for first time user comes to EnterCustomer screen just bind it to empty customer object
+            //customer object is for single object which will bind with textboxes
+            //customers collection is a collection which will bind with a table
+            customerViewModel.customer = new Customer();
+
+            //using CustomerDAL filling customers Collection
+            CustomerDAL objCustomer = new CustomerDAL();//to fetch data from database
+
+            //populating data 
+            List<Customer> customersCollection = objCustomer.Customers.ToList<Customer>();
+
+            //passing customersCollection to represent data in grid: customers
+            customerViewModel.customers = customersCollection;
+
+            //passing this complete to EnterCustomer screen
+            return View("EnterCustomer", customerViewModel);
+
+            //to call EnterCustomer screen we cannot only pass Customer object we need to pass view model
+            //return View("EnterCustomer", new Customer());
         }
 
-        public ActionResult Submit(Customer obj)//Customer class represents middle layer
+        //EnterSearch() will invoke Search() customer screen
+        public ActionResult EnterSearch()
+        {
+            CustomerViewModel customerViewModel = new CustomerViewModel();
+            customerViewModel.customers = new List<Customer>();
+            return View("SearchCustomer", customerViewModel);
+        }
+
+        //Search() action will actually do a search
+        public ActionResult SearchCustomer()
+        {
+            CustomerViewModel customerViewModel = new CustomerViewModel();
+            CustomerDAL dalCustomer = new CustomerDAL();//to fetch data from database
+            string str = Request.Form["txtCustomerName"].ToString();
+            List<Customer> customersCollection =
+                (from x in dalCustomer.Customers
+                 where x.CustomerName == str
+                 select x).ToList<Customer>();
+            customerViewModel.customers = customersCollection;
+            return View("SearchCustomer", customerViewModel);
+
+        }
         //public ActionResult Submit([ModelBinder(typeof(CustomerBinder))]
         //    Customer obj)//In case name of textbox i.e. UI and property name of model class are same than we can dirctly put object like this and 
         //                                        //Request.Form is done directly by MVC framework itself
+
+        //Customer object was polulating by itself. Automatic binding won't happen now so we have to map objects manually
+        //and this because model structure is changed and as model structure is changed text box names are changed as
+        //CustomerName has chnaged to Customer.CustomerName and CustomerCode has changed to Customer.CustomerCode
+        //public ActionResult Submit(Customer obj)//Customer class represents middle layer
+        public ActionResult Submit()
         {
-            if(ModelState.IsValid)
+            CustomerViewModel customerViewModel = new CustomerViewModel();
+
+            Customer objCustomer = new Customer();
+
+            //filling customer objects by proper textbox names
+            //Proper way to do below is to use model binders
+            objCustomer.CustomerName = Request.Form["Customer.CustomerName"];
+            objCustomer.CustomerCode = Request.Form["Customer.CustomerCode"];
+
+            if (ModelState.IsValid)
             {
                 //insert the customer object to database using Entity framework DAL
                 CustomerDAL customerDAL = new CustomerDAL();
-                customerDAL.Customers.Add(obj);//in-memory commit
+                customerDAL.Customers.Add(objCustomer);//in-memory commit
                 customerDAL.SaveChanges();//physical commit to database
 
-                return View("Customer", obj);
+                //return View("Customer", obj);
+                //if Customer object -> objCustomer is valid and it's saved to database
+                //than single view model customer has to be set new Customer object.
+                customerViewModel.customer = new Customer();
             }
             else
             {
-                return View("EnterCustomer", obj);//EnterCustomer is a UI
+                //if customer object -> objCustomer is not valid we have to persist CustomerName and CustomerCode
+                customerViewModel.customer = objCustomer;
             }
+            //finally we need to load the customers collection
+            CustomerDAL dalCustomer = new CustomerDAL();//to fetch data from database
+            List<Customer> customersCollection = dalCustomer.Customers.ToList<Customer>();
+            customerViewModel.customers = customersCollection;
+
+            //As soon as Submit() is called -> EnterCustomer will be called irrespective of errors
+            //EnterCustomer should not be binded with objCustomer which is a Customer object but it should be binded with
+            //customerViewModel object
+            //return View("EnterCustomer", obj);
+
+            //passing customerViewModel object instead of objCustomer which is a Customer object
+            return View("EnterCustomer", customerViewModel);
+            //else
+            //{
+            //    return View("EnterCustomer", obj);//EnterCustomer is a UI
+            //}
+
             //Customer obj = new Customer();
             //obj.CustomerCode = Request.Form["CustomerCode"];
             //obj.CustomerName = Request.Form["CustomerName"];
---
+++ HelloWorld/HelloWorld/ViewModel/CustomerViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using HelloWorld.Models;//As CustomerViewModel internally uses Model
+
+namespace HelloWorld.ViewModel
+{
+    /// <summary>
+    /// This CustomerViewModel class represents Customer data as well as list of customer.
+    /// CustomerViewModel class has two models : 1) customer 2) List of customers
+    /// customer will be attached two text boxes: Customer Name and Customer Code
+    /// customers will help to represent data into the grid i.e. list of customers
+    /// </summary>
+    public class CustomerViewModel
+    {
+        //Customer represent customer object
+        public Customer customer { get; set; }
+
+        //customers represents List of customers objects
+        public List<Customer> customers { get; set; }
+    }
+}
